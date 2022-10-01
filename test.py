25048e9ee1f0f50472944075fbfeec9d7e3a20cb
Ann Slavinskaya <annslavinskaya@MacBook-Air-Ann.local>
2022-10-01 22:31:55 +0300

    dsf

---
+++ test.py
@@ -0,0 +1,18 @@
+import math
+print(math.__doc__)
+math.sqrt.__doc__
+
+class Test:
+    """Класс Test используется для демонстрации docstring."""
+
+    def first(self):
+        """Описывает метод first и демонстрирует перенос строки 
+        документации.
+        """
+        
+
+
+print(__doc__)
+print(tricky_func.__doc__)
+print(Test.__doc__)
+print(Test.first.__doc__) 
